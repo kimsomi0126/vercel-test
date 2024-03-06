07eba4c6fa7db66b00fbf47820a9c40f51f81715
kimsomi0126 <kimsomi00126@gmail.com>
2024-03-06 12:54:13 +0900

    240306-1 배포

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./router/root";
-
-function App() {
-  return <RouterProvider router={router} />;
-}
-export default App;
--- src/util/utils.js
+++
@@ -1,6 +0,0 @@
-export const getNum = (체크, 기본) => {
-  if (!체크) {
-    return 기본;
-  }
-  return 체크;
-};
